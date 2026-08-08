c1b4a699f176f563df0d09fff8c07de6cdfa3bc6
Indrajeet khatarkar <indrajeetkhatarkar5@gmail.com>
2026-08-08 16:23:31 +0530

    Fix production build errors

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { useCallback } from "react";
 import Particles from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
-import type { Engine } from "@tsparticles/engine";
 
 export default function ParticlesBackground() {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadSlim(engine);
-  }, []);
-
   return (
     <Particles
       id="particles"
-      init={particlesInit}
       options={{
         fullScreen: {
           enable: false,
